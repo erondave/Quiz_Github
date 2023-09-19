19102d8774d9de1b8a6bf0c587b6c9c8bc379ff2
erondave <erondave26@gmail.com>
2023-09-19 16:00:47 -0700

    first commit

--- quizz/src/Helpers/QuestionBank.js
+++ quizz/src/Helpers/QuestionBank.js
@@ -5,30 +5,29 @@ export const Questions = [
         optionB: "6",
         optionC: "3",
         optionD: "4",
-        answer: "A"
-        
-
-    },
-    {
-        prompt: "What is 2+2",
-        optionA: "5",
-        optionB: "6",
-        optionC: "3",
-        optionD: "4",
-        answer: "B"
-        
-
-    },
-    {
-        prompt: "What is 2+2",
-        optionA: "5",
-        optionB: "6",
-        optionC: "3",
-        optionD: "4",
         answer: "D"
         
 
     },
+    {
+        prompt: "What is 5+2",
+        optionA: "5",
+        optionB: "6",
+        optionC: "7",
+        optionD: "4",
+        answer: "C"
+        
+
+    },
+    {
+        prompt: "What is 2+3",
+        optionA: "5",
+        optionB: "6",
+        optionC: "3",
+        optionD: "4",
+        answer: "A"
+        
+
+    },
 ]
 
-]
